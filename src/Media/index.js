49992b7d6cb7adaf4d279b46d787a738d5f49102
jxom <jakemoxey@gmail.com>
2018-04-14 20:57:38 +1000

    fix

--- src/Media/index.js
+++ src/Media/index.js
@@ -1,6 +1,6 @@
 export { default as Media } from './Media';
 export { default as MediaLeft } from './MediaLeft';
 export { default as MediaRight } from './MediaRight';
-export { default as MediaItem } from './MediaItem';
+export { default as MediaContent } from './MediaContent';
 export { default as MediaImage } from './MediaImage';
 export { default } from './Media';
