ede454971a9c5173865cc637b78df9bdfd758028
jxom <jakemoxey@gmail.com>
2018-05-14 21:41:45 +1000

    Amend index modules

---
+++ src/_common/index.js
@@ -0,0 +1,5 @@
+export { default as Field } from './Field';
+export { default as FieldBody } from './FieldBody';
+export { default as FieldControl } from './FieldControl';
+export { default as FieldLabel } from './FieldLabel';
+export { default as FieldText } from './FieldText';
